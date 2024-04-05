358259a47d471f8d2d4a7a82d264461315d7e648
satyasairam.annamreddi <68277614+satyasairam3@users.noreply.github.com>
2024-04-05 10:34:04 +0530

    Update gitconfig.sh

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,4 +1,4 @@
 git config --global user.email "assrjt003@gmail.com"
 git config --global user.name "satyasairam3"
 
-git clone https://github.com/satyasairam3/node-workspace.git
+git pull https://github.com/satyasairam3/node-workspace.git
